9ef78a5fd925dfeddf5d38268ffb54512be15bf4
AlexisBo <aboisne77@gmail.com>
2018-04-09 23:16:36 +0200

    corrections fautes

--- Fotoen/src/controleur/GestionAlbums.java
+++ Fotoen/src/controleur/GestionAlbums.java
@@ -38,7 +38,7 @@ public class GestionAlbums extends HttpServlet {
 		traitement(request, response);
 	}
 
-	//traite les différente actions exercées sur la vue (suppréssion, ajout, ...)
+	//traite les différentes actions exercées sur la vue (suppréssion, ajout, ...)
 	public void traitement(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
--- Fotoen/src/controleur/GestionPhotos.java
+++ Fotoen/src/controleur/GestionPhotos.java
@@ -40,7 +40,7 @@ public class GestionPhotos extends HttpServlet {
 		traitement(request, response);
 	}
 
-	//traite les différente actions exercées sur la vue (suppréssion, ajout, consultation, visualisation, ...)
+	//traite les différentes actions exercées sur la vue (suppréssion, ajout, consultation, visualisation, ...)
 	public void traitement(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
