5870a00429b060c7cbab3b602c16bca993f521dc
AlexisBo <aboisne77@gmail.com>
2018-04-17 12:50:23 +0200

    commentaires entités

--- Fotoen/src/modele_entity/Commentaire.java
+++ Fotoen/src/modele_entity/Commentaire.java
@@ -21,6 +21,10 @@ public class Commentaire {
 		this.date = new Date(new java.util.Date().getTime());
 	}
 
+	/**
+	 * @param commentaire, evaluation, idUtilisateur, idMedia
+	 *            the commentaire, evaluation, idUtilisateur, idMedia to set
+	 */
 	private void setCommentaire(String commentaire, int evaluation, int idUtilisateur, int idMedia) {
 		this.commentaire = commentaire;
 		this.evaluation = evaluation;
@@ -28,50 +32,92 @@ public class Commentaire {
 		this.setIdMedia(idMedia);
 	}
 
+	/**
+	 * @return id
+	 */
 	public int getId() {
 		return this.id;
 	}
 
+	/**
+	 * @param id
+	 *            the id to set
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return commentaire
+	 */
 	public String getCommentaire() {
 		return this.commentaire;
 	}
 
+	/**
+	 * @param commentaire
+	 *            the commentaire to set
+	 */
 	public void setCommentaire(String commentaire) {
 		this.commentaire = commentaire;
 	}
 
+	/**
+	 * @return evaluation
+	 */
 	public int getEvaluation() {
 		return evaluation;
 	}
 
+	/**
+	 * @param evaluation
+	 *            the evaluation to set
+	 */
 	public void setEvaluation(int evaluation) {
 		this.evaluation = evaluation;
 	}
 
+	/**
+	 * @return idUtilisateur
+	 */
 	public int getIdUtilisateur() {
 		return idUtilisateur;
 	}
 
+	/**
+	 * @param idUtilisateur
+	 *            the idUtilisateur to set
+	 */
 	public void setIdUtilisateur(int idUtilisateur) {
 		this.idUtilisateur = idUtilisateur;
 	}
 
+	/**
+	 * @return idMedia
+	 */
 	public int getIdMedia() {
 		return idMedia;
 	}
 
+	/**
+	 * @param idMedia
+	 *            the idMedia to set
+	 */
 	public void setIdMedia(int idMedia) {
 		this.idMedia = idMedia;
 	}
 
+	/**
+	 * @return date
+	 */
 	public Date getDate() {
 		return date;
 	}
 
+	/**
+	 * @param date
+	 *            the date to set
+	 */
 	public void setDate(Date date) {
 		this.date = date;
 	}
